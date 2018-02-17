3ed574ff88b7238378f387e4cdd9067badfb88e5
JcShang <jiacshang@gmail.com>
2018-02-17 14:20:28 -0500

    Location part finished

--- app/src/main/java/com/tucis/jiacheng/redpackets/MainActivity.java
+++ app/src/main/java/com/tucis/jiacheng/redpackets/MainActivity.java
@@ -67,6 +67,31 @@ public class MainActivity extends Activity implements View.OnClickListener{
         else{
             System.out.println("No permission");
             //need extra code to grant the permission
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET},
+                            1);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
         }
     }
 
